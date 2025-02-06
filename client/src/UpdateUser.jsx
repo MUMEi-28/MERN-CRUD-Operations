64f8220ae7a3f9e3962ae8d0c1f6246514b1d3d5
Marc Jersey Castro <marcjerseycastro@gmail.com>
2025-02-06 21:46:36 +0800

    react form variable

--- client/src/UpdateUser.jsx
+++ client/src/UpdateUser.jsx
@@ -3,29 +3,8 @@ import React from "react";
 export default function UpdateUser()
 {
     return (
-        <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
-            <div className="w-50 bg-white rounded p-3">
-                <form action="">
-                    <h2>Update User</h2>
-
-                    <div className="mb-2">
-                        <label htmlFor="">Name</label>
-                        <input type="text" placeholder="Enter Name" className="form-control" />
-                    </div>
-
-                    <div className="mb-2">
-                        <label htmlFor="">Email</label>
-                        <input type="text" placeholder="Enter Email" className="form-control" />
-                    </div>
-
-                    <div className="mb-2">
-                        <label htmlFor="">Age</label>
-                        <input type="text" placeholder="Enter Age" className="form-control" />
-                    </div>
-
-                    <button className="btn btn-success ms-2">Update</button>
-                </form>
-            </div>
+        <div>
+            Update User
         </div>
     )
 }
